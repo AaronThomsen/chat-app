e3d4c0e9e9dd4fc18a80c3693d4582994250cde6
Aaron Thomsen <aarontthomsen@gmail.com>
2018-09-07 03:24:21 -0700

    Heroku config

--- backend/server.js
+++ backend/server.js
@@ -157,9 +157,9 @@ io.on('connection', (socket) => {
   });
 });
 
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
-});
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
+// });
 
 server.listen(port, () => {
   console.log(`Now listening on port ${port}`);
